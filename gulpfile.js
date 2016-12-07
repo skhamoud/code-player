0052d55d53cd0b7f80eff590bb1c4266296c1823
Hamoud SK <skhamoud@gmail.com>
2016-12-07 23:37:44 +0000

    Restructure project

--- gulpfile.js
+++ gulpfile.js
@@ -2,54 +2,63 @@ var gulp = require('gulp');
 var sass = require("gulp-sass");
 var browserSync = require("browser-sync").create();
 var autoprefixer= require("gulp-autoprefixer");
-// tasks
+var merge = require('merge-stream');
 
-/* Deals with styles tasks (compiles sass to css , adds prefixes,
-* puts in destination) and then reloads with browser-sync
-*/
-gulp.task("styles", function () {
-  return gulp.src("assets/sass/main.scss")
+let dependencies = {
+  css : ["node_modules/bootstrap/dist/css/bootstrap.min.css",
+        "node_modules/font-awesome/css/font-awesome.min.css",
+        "node_modules/animate.css/animate.min.css"],
+  js  : ["node_modules/jquery/dist/jquery.min.js",
+        "node_modules/bootstrap/dist/js/bootstrap.min.js"]
+};
+
+
+let src = {
+  sass : "assets/sass/",
+  js : "assets/js/",
+  img :  "assets/img/"
+};
+let public = {
+  css : 'public/css/',
+  js : "public/js/"
+};
+
+
+//============= Tasks ====================
+
+// Run Once After Installation If Dev Env is offline
+// And Use CDN for production
+gulp.task('copyDependencies', function() {
+  var cssDep = gulp.src(dependencies.css)
+              .pipe(gulp.dest(public.css));
+  var jsDep = gulp.src(dependencies.js)
+              .pipe(gulp.dest(public.js));
+  return merge(cssDep, jsDep);
+});
+
+gulp.task("styles", ()=> {
+  return gulp.src(src.sass +"main.scss")
 .pipe(sass().on('error', sass.logError))
 .pipe(autoprefixer())
-.pipe(gulp.dest("assets/css"))
+.pipe(gulp.dest(public.css))
 .pipe(browserSync.reload({stream:true}));
 });
 
-// copy files
-gulp.task("copy", function(){
-  return gulp.src(/* put here files to copy like this ==> */ ['assets/css/main.css','assets/js/custom.js','index.html','CodePlayer.html'])
-  .pipe(gulp.dest('./dist')/* destination folder*/);
-});
-
-// just the sass ()
-gulp.task("sass" , function () {
-  return gulp.src('assets/sass/main.scss')
-      .pipe(sass().on('error' , sass.logError))
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('assets/css'));
-});
-
-gulp.task('watch-sass' , ['sass'] , function(){
-  gulp.watch('assets/sass/*' , ['sass']);
-});
-
 // Serve files from base directory
-gulp.task('serve', function() {
+gulp.task('serve', ['styles'] , function() {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './public/'
     }
   });
 });
 
 // watches files and manually reloads after  files change
-
-gulp.task('watch', ['serve'], function () {
-  gulp.watch("assets/**/*.scss", ['styles']);
-  gulp.watch("assets/**/*.svg", ['styles']);
-  gulp.watch('./**/*.html').on('change', browserSync.reload);
-  gulp.watch('assets/**/*.js').on('change', browserSync.reload);
+gulp.task('watch', function () {
+  gulp.watch(src.sass + "*", ['styles']);
+  gulp.watch(public + '*.html').on('change', browserSync.reload);
+  gulp.watch(public.js + 'main.js').on('change', browserSync.reload);
 });
 
 
-gulp.task("default", ['styles', 'watch']);
+gulp.task("default", ['serve', 'watch']);
